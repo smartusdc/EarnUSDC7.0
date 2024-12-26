d42201f02a8809455251dad80c7d681ff21f940c
smartusdc <jakepaul900009@gmail.com>
2024-12-26 15:21:23 +0900

    Update EarnUSDC.js

--- components/EarnUSDC.js
+++ components/EarnUSDC.js
@@ -85,6 +85,20 @@ const CONTRACT_ABI = [
         "stateMutability": "view",
         "type": "function"
     }
+    {
+        "inputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "name": "userToReferralCode",
+        "outputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "stateMutability": "view",
+        "type": "function"
+    },
+    {
+        "inputs": [{"internalType": "uint256", "name": "", "type": "uint256"}],
+        "name": "referralCodeToUser",
+        "outputs": [{"internalType": "address", "name": "", "type": "address"}],
+        "stateMutability": "view",
+        "type": "function"
+    }
 ];
 
 const USDC_ABI = [
